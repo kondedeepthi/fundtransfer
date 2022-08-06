6472923837bf185d9923eda5c1fe29779c11795f
kondedeepthi <deepthi.kondae@gmail.com>
2022-08-06 12:29:19 +0400

    initial commit

---
+++ src/test/java/com/hclhackathon/fundtransfer/FundtransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hclhackathon.fundtransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundtransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
